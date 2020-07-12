df1fc181ab0d1a223e0754a447d0806d8eff438b
V-Eka <67388182+V-Eka@users.noreply.github.com>
2020-07-12 19:32:17 +0300

    Update bot_for_yandex.js

--- bot_for_yandex.js
+++ bot_for_yandex.js
@@ -9,35 +9,34 @@
 // ==/UserScript==
 
 
-let keywords = ["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"];
-let keyword  = keywords[getRandom(0,keywords.length)];
-let btnY = document.querySelectorAll('[type="submit"]')[0];
+let keywords = ["Гобой", "Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель"];
+let keyword = keywords[getRandom(0, keywords.length)];
+let btnYandex = document.querySelectorAll('[type="submit"]')[0];
 
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
-      }
-
+}
 function writeKeyword(word){
-    let i = 0;
-    let timerId = setInterval( ()=>{
-        document.getElementsByClassName("input__control input__input")[0].value+=word[i];
+    let i= 0;
+    let timerId = setInterval(()=>{
+        document.getElementsByClassName("input__control input__input")[0].value+= word[i];
         i++;
         if(i==word.length){
             clearInterval(timerId);
-            btnY.click();
+            btnYandex.click();
         }
-      }, 1000);
-     }
+    },1000);
+}
 
 
-if (btnY != undefined)
+if (btnYandex != undefined)
     writeKeyword(keyword);
-   else{
-     let links = document.links;
+else{
+    let links = document.links;
     for(let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
             links[i].click();
             break;
-           }
-       }
-      }
+        }
+    }
+}
