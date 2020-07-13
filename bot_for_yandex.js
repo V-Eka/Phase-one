02379597406f4b2f8934dab14e937602e065f45c
V-Eka <67388182+V-Eka@users.noreply.github.com>
2020-07-13 15:57:33 +0300

    Update bot_for_yandex.js
    1. исправление ошибок (условие для нажатия кнопки "Найти" - только если находишься на странице ПОИСКА, а не раздачи. Раздача определена отсутствием главного логотипа поисковика; ПРАВИЛО отмены "target = "_blank"".
    2. программа вводит в поисковую строку искомые слова/фразы из массива, переходит в раздачу, отбирает нужный адрес, ориентируясь на "href", переходит на нов. сайт, листает внутри него ссылки, после "самостоятельно" покидает его, чтобы снова пойти в начало - ввод следующего запроса в строку "Поиск".
    3. с помощью get.Random+Math.random имитируется поведенческий фактор пользователя.

--- bot_for_yandex.js
+++ bot_for_yandex.js
@@ -5,38 +5,53 @@
 // @description  try to take over the world!
 // @author       You
 // @match       https://yandex.ru/*
+// @match       https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-
-let keywords = ["Гобой", "Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель"];
+let keywords = ["Гобой", "Как звучит флейта", "Тромбон", "Что такое валторна","Музыкальные диктанты", "Фагот", "Скрипка", "Виолончель", "Различаем тембры"];
 let keyword = keywords[getRandom(0, keywords.length)];
 let btnYandex = document.querySelectorAll('[type="submit"]')[0];
-
+let links = document.links;
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
 function writeKeyword(word){
     let i= 0;
     let timerId = setInterval(()=>{
-        document.getElementsByClassName("input__control input__input")[0].value+= word[i];
+        text.value+= word[i];
         i++;
         if(i==word.length){
             clearInterval(timerId);
             btnYandex.click();
         }
-    },1000);
+    },500);
 }
-
-
-if (btnYandex != undefined)
+if (document.getElementsByClassName("home-logo__default")[0] != undefined)
     writeKeyword(keyword);
-else{
-    let links = document.links;
+else if (location.hostname=="yandex.ru"){
+    let flag = true;
     for(let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+            links[i].removeAttribute('target');
+            flag = false;
             links[i].click();
             break;
         }
     }
+    if (document.getElementsByClassName("pager__item pager__item_kind_page")[0].textContent > 9){
+        flag = false;
+        location.href = "https://yandex.ru/";
+    }
+    if(flag) setTimeout(()=>{document.getElementsByClassName("pager__item pager__item_kind_next")[0].click()},getRandom(3000,6000));
+}else{
+    setInterval(()=>{
+        if(getRandom(0,101)>=70)location.href = "https://yandex.ru/";
+        else{
+            let index = getRandom(0,links.length);
+            if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1);
+               links[index].click();
+        }
+    },getRandom(2000,6000));
 }
+
