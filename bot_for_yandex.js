c7820a7ef8cc65690426f23294cc23ff209be624
V-Eka <67388182+V-Eka@users.noreply.github.com>
2020-07-14 14:17:10 +0300

    14.07/2020
    внесение изменений: расширение кол-ва "читаемых" сайтов; сохранение cookie в коде для этих сайтов;  определение переменной site глобально и локально для использования в каждом шаге кода по правилам этого "шага". Оставлена комментарием переменная let link = links[i], т.к. работает без её добавления (однако!: она нужна для "клика" по выбранному  случайно адресу на странице "раздачи", т.к. из-за setTimeout есть вероятность, что нужный адрес "сдвинется" в строке "раздачи" со стороны браузера за тот период времени, который установлен для имитации человека)

--- bot_for_yandex.js
+++ bot_for_yandex.js
@@ -6,16 +6,20 @@
 // @author       You
 // @match       https://yandex.ru/*
 // @match       https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match       https://crushdrummers.ru/*
+// @match       https://dkzelenograd.ru/*
+// @match       https://www.zelenograd.ru/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["Гобой", "Как звучит флейта", "Тромбон", "Что такое валторна","Музыкальные диктанты", "Фагот", "Скрипка", "Виолончель", "Различаем тембры"];
-let keyword = keywords[getRandom(0, keywords.length)];
+
 let btnYandex = document.querySelectorAll('[type="submit"]')[0];
 let links = document.links;
+let site = location.host;
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
+
 function writeKeyword(word){
     let i= 0;
     let timerId = setInterval(()=>{
@@ -27,15 +31,38 @@ function writeKeyword(word){
         }
     },500);
 }
-if (document.getElementsByClassName("home-logo__default")[0] != undefined)
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+ //ниже "если" - со страницы старта - Яндекс
+if (document.getElementsByClassName("home-logo__default")[0] != undefined){
+    let sites = {
+     "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Как звучит флейта", "Музыкальные диктанты", "Фагот", "Скрипка", "Виолончель", "Различаем тембры"],
+     "crushdrummers.ru":["Шоу барабанщиков", "Барабанное шоу", "Crash барабанщики"],
+     "dkzelenograd.ru":["Афиша", "Мастер-классы", "Выставки", "Творческие коллективы", "Секции", "Изображулька"],
+      "www.zelenograd.ru": ["Новости города", "Крюковский аэродром и его герои", "Дом лани", "Снятие ограничений", "Alma mater Зеленограда"],
+};
+    let site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
+    let keywords = sites[site];
+    let keyword = keywords[getRandom(0, keywords.length)];
+    document.cookie = "site="+site;
     writeKeyword(keyword);
+}
+//далее "иначе если" - страница ПОИСКОВОЙ выдачи Яндекс (location.hostname)!!!!
 else if (location.hostname=="yandex.ru"){
     let flag = true;
+    let site = getCookie("site");
     for(let i=0; i<links.length; i++){
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+        if(links[i].href.indexOf(site)!=-1){
             links[i].removeAttribute('target');
+   //       let link = links[i];
             flag = false;
-            links[i].click();
+            setTimeout(()=>{links[i].click()},getRandom(1000, 3000));
             break;
         }
     }
@@ -44,14 +71,17 @@ else if (location.hostname=="yandex.ru"){
         location.href = "https://yandex.ru/";
     }
     if(flag) setTimeout(()=>{document.getElementsByClassName("pager__item pager__item_kind_next")[0].click()},getRandom(3000,6000));
-}else{
+}
+// "иначе" - мы находимся на искомом сайте
+else{
     setInterval(()=>{
         if(getRandom(0,101)>=70)location.href = "https://yandex.ru/";
         else{
             let index = getRandom(0,links.length);
-            if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1);
+            if(links[index].href.indexOf(site)!=-1);
                links[index].click();
         }
     },getRandom(2000,6000));
 }
+
 
